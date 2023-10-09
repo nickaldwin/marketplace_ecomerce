8cdc1a7c7ede008b817aa473f97cf6037731910d
nickaldwin <nicklemoncito98@gmail.com>
2023-10-09 10:30:41 +0800

    update files

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# marketplace_ecomerce
+# E-Commerce Web Shop App
+ An online store
+<p align="center">
+    <img src="https://raw.githubusercontent.com/emreharman/e-commerce/master/src/assets/published.gif">
+</p>
+
+<h2> 1. Cloning and Running App :computer:</h2>
+
+
+
+You can download this app to your local using `git clone https://github.com/emreharman/e-commerce.git` command on your bash. Then open your terminal in the folder that you cloned and open your command line, use `yarn install` command. This command will install all the dependencies that I use in the app. Finally use `yarn start` command to run the app.
+
+<h2> 2. About App :warning: </h2>
+
+
+
+This app is the simulation of an e-commerce site. You can enter a test credit card as I use on the gif, just keep pressing 4 and 2. I didn't use any database to store any information. I've used commerce.js to handle cart and checkout operations. Also I've stored the info's of products in the commerce.js. It's very useful to use commerce.js for storing your product's info's and i can handle all of cart and checkout operations.In this way, you don't need to write any backend or database or etc.
+
+<h2> 3. Dependencies :open_file_folder:</h2>
+
+
+
+In this app I've used **_Material UI_**, **_Commerce.js_**, **_Stripe.js_**, **_React-Router_** and **_React-Hooks_**.
+There are the commands that you can use to install these dependencies. (I've used yarn, you can use `npm install` instead of `yarn add` if you're using npm).
+
+- ` yarn add @material-ui/core @material-ui/icons`
+- `yarn add @chec/commerce.js`
+- `yarn add @stripe/react-stripe-js @stripe/stripe-js`
+- `yarn add react-router-dom`
+- `yarn add react-hook-form`
---
+++ src/components/CheckoutForm/PaymentForm.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import {Typography,Button,Divider} from "@material-ui/core";
+import {Elements,CardElement,ElementsConsumer} from "@stripe/react-stripe-js";
+import {loadStripe} from "@stripe/stripe-js";
+import Review from "./Review";
+
+const PaymentForm=({shippingData,checkoutToken,backStep,onCaptureCheckout,nextStep})=>{
+
+    const stripePromise=loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
+    const handleSubmit=async (event,elements,stripe)=>{
+        event.preventDefault();
+        
+        if(!stripe || !elements) return;
+
+        const cardElement=elements.getElement(CardElement);
+        const {error,paymentMethod}=await stripe.createPaymentMethod({type:'card',card:cardElement});
+
+        if(error){
+            console.log(error);
+        }else{
+            const orderData={
+                line_items:checkoutToken.live.line_items,
+                customer:{firstname:shippingData.firstName, lastname:shippingData.lastName,email:shippingData.email,customer_reference:`${shippingData.firstName}${shippingData.lastName}${new Date().toLocaleDateString()}`},
+                shipping:{
+                    name:"Primary",
+                    street:shippingData.address1,
+                    town_city:shippingData.city,
+                    country_state:shippingData.shippingSubdivision,
+                    postal_zip_code:shippingData.zip,
+                    country:shippingData.shippingCountry
+                },
+                
+                payment:{
+                    gateway:'stripe',
+                    stripe:{
+                        payment_method_id:paymentMethod.id
+                    }
+                }
+            }
+            onCaptureCheckout(checkoutToken.id,orderData);
+            timeout();
+            nextStep();
+        }
+    }
+    const timeout=()=>{
+                setTimeout(() => {
+                    console.log("spinning");
+                }, 2000);
+            }
+    return (
+        <React.Fragment>
+            <Review checkoutToken={checkoutToken}/>
+            <Divider/>
+            <Typography variant="h6" gutterBottom style={{margin:"20px 0"}}>Payment Method</Typography>
+            <Elements stripe={stripePromise}>
+                <ElementsConsumer>
+                    {({elements,stripe})=>(
+                        <form onSubmit={(e)=>handleSubmit(e,elements,stripe)}>
+                            <CardElement/>
+                            <br/><br/>
+                            <div style={{display:'flex',justifyContent:'space-between'}}>
+                                <Button variant="outlined" onClick={backStep}>Back</Button>
+                                <Button type="submit" variant="contained" color="primary" disabled={!stripe}>
+                                    Pay {checkoutToken.live.subtotal.formatted_with_symbol}
+                                </Button>
+                            </div>
+                        </form>
+                    )}
+                </ElementsConsumer>
+            </Elements>
+        </React.Fragment>
+    );
+}
+
+export default PaymentForm;
